c021af68adb41f95a0657a489dd3bac22b51a645
patriciaderieux <6089242+patty345@users.noreply.github.com>
2022-04-02 13:42:44 -0500

    fixed the thought controller

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -7,7 +7,7 @@ const {
   deleteThought,
   addReaction,
   removeReaction,
-} = require('../../controllers/thought-controller');
+} = require('../../controllers/tought-controller');
 
 // /api/thoughts
 router.route('/').get(getThoughts).post(createThought);
