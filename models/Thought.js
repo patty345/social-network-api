041ae27de22a3862e0f4688dadb8b6733ea2327a
patriciaderieux <6089242+patty345@users.noreply.github.com>
2022-04-02 13:31:12 -0500

    redo of models files

--- models/Thought.js
+++ models/Thought.js
@@ -3,34 +3,34 @@ const moment = require('moment');
 const reactionSchema = require('./Reaction');
 
 const thoughtSchema = new Schema(
-    {
-        thoughtText: {
-            type: String,
-            required: 'A thought is required',
-            minlength: 1,
-            maxlength: 280,
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            get: (timestamp) => moment(timestamp).format('MMM Do, YYYY [at] hh:mm a'),
-        },
-        username: {
-            type: String,
-            required: true,
-        },
-        reactions: [reactionSchema],
+  {
+    thoughtText: {
+      type: String,
+      required: 'A thought is required',
+      minlength: 1,
+      maxlength: 280,
     },
-    {
-        toJSON: {
-            getters: true,
-        },
-        id: false,
-    }
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => moment(timestamp).format('MMM Do, YYYY [at] hh:mm a'),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
 );
 
 thoughtSchema.virtual('reactionCount').get(function () {
-    return this.reactions.length;
+  return this.reactions.length;
 });
 
 const Thought = model('Thought', thoughtSchema);
--- models/User.js
+++ models/User.js
@@ -1,42 +1,42 @@
 const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema(
-    {
-        username: {
-            type: String,
-            required: true,
-            unique: true,
-            trim: true,
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-            match: [/.+@.+\..+/, 'Must match an email address!'],
-        },
-        thoughts: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: 'Thought',
-            },
-        ],
-        friends: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: 'User',
-            },
-        ],
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
     },
-    {
-        toJSON: {
-            virtuals: true,
-        },
-        id: false,
-    }
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Must match an email address!'],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
 );
 
 userSchema.virtual('friendCount').get(function () {
-    return this.friends.length;
+  return this.friends.length;
 });
 
 const User = model('User', userSchema);
